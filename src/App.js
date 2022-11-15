6b6bb7d52856df1be242e74e8b562f8ae7c16f89
Alberto <albertoflj@gmail.com>
2022-11-15 19:04:05 +0200

    Fixed deployment bug.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,50 @@ import "./scss/App.scss";
 import { useEffect, useState } from "react";
 
 function App() {
+    //dummy data in case local storage is empty
+    let dummyData = {
+      items: [
+        {
+          id: 1,
+          content:"Complete online JavaScript course",
+          checked: false,
+        },
+        {
+          id: 2,
+          content:"Jog around the park 3x",
+          checked: false,
+        },
+        {
+          id: 3,
+          content:"10 minutes meditation",
+          checked: false,
+        },
+        {
+          id: 4,
+          content:"Read for 1 hour",
+          checked: false,
+        },
+        {
+          id: 5,
+          content:"Pick up groceries",
+          checked: false,
+        },
+        {
+          id: 6,
+          content:"Complete Todo App on Frontend Mentor",
+          checked: false,
+        },
+      ]
+    }
+  
+    //if user visits the page for the first time, set dummy data to local storage
+    if(localStorage.getItem("firstTimeVisiting") === null){
+      localStorage.setItem("firstTimeVisiting", "false");
+      localStorage.setItem("items", JSON.stringify(dummyData.items));
+      localStorage.setItem("currentCategory", "all");
+      localStorage.setItem("theme", "light");
+    }
+  
   const [theme, setTheme] = useState(localStorage.getItem("theme"));
   function onStorageChange(e) {
     setTheme(localStorage.getItem("theme"));
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,6 +6,58 @@ import Categories from "./to-do-components/Categories";
 
 
 function ToDo(props) {
+
+  //dummy data in case local storage is empty
+  let dummyData = {
+    items: [
+      {
+        id: 1,
+        content:"Complete online JavaScript course",
+        checked: false,
+      },
+      {
+        id: 2,
+        content:"Jog around the park 3x",
+        checked: false,
+      },
+      {
+        id: 3,
+        content:"10 minutes meditation",
+        checked: false,
+      },
+      {
+        id: 4,
+        content:"Read for 1 hour",
+        checked: false,
+      },
+      {
+        id: 5,
+        content:"Pick up groceries",
+        checked: false,
+      },
+      {
+        id: 6,
+        content:"Complete Todo App on Frontend Mentor",
+        checked: false,
+      },
+    ]
+  }
+
+  //if user visits the page for the first time, set dummy data to local storage
+  if(localStorage.getItem("firstTimeVisiting") === null){
+    localStorage.setItem("firstTimeVisiting", "false");
+    localStorage.setItem("items", JSON.stringify(dummyData.items));
+    localStorage.setItem("currentCategory", "all");
+    localStorage.setItem("theme", "dark");
+  }
+
+
+
+
+
+
+
+
   return (
     <div className="main-app width-limit">
       <Header theme={props.theme} />
